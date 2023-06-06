25a2f3e3ff781e332e77b7d2e67b55ea6f900f52
Federico C <fede03billy@gmail.com>
2023-06-06 17:25:43 +0200

    ⚙️ format script with rome

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 // App.tsx
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import User from "./components/User";
